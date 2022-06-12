42356608f2ecb2b8f6d7332fea424994e7a31d82
MRX <arsenber09@gmail.com>
2022-06-13 01:09:07 +0300

    main

--- INSTALL.py
+++ INSTALL.py
@@ -10,8 +10,17 @@ if (main_disk == ""): main_disk = "C"
 else: main_disk = main_disk
 
 
-config_file = f"{main_disk}:\\\\Windows\\wurpm-config.py"
-# os.system(f"")
-# print(config_file)
+
+    
+try:    
+    config_file = f"{main_disk}:/Windows/WURPM/wurpm-config.py"
+    print(config_file)
+    os.system(f"cd {config_file}")
+    # print(config_file)
+    # pass
+
+except:
+    print("Config File not Found!\nConfig File created...")
+
 
 
