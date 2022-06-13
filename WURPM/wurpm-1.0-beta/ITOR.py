765c6203e31b45c5ba91115c6d27f399b8c1453e
MRX-1T <97108901+Arsen-Ber@users.noreply.github.com>
2022-06-13 20:37:16 +0300

    Add files via upload

---
+++ WURPM/wurpm-1.0-beta/ITOR.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+
+
+
+WURPM_version = 1.0
+user = os.getlogin()
+
+
+
+# TOEACH: ITAR = Install, Tune Or Remove  (wurpm)
+
+def main():
+
+    main_disk = input( "Enter name for first disk on PC (standart=C): " )
+    if (main_disk == ""): main_disk = "C"
+    else: main_disk = main_disk
+
+
+    param = input("Choose action.\n1. Create config dir / file \n2. None \n3. None \n>>> ")
+
+
+    config_dir = f"{main_disk}:\\Users\\{user}\\WURPM-{WURPM_version}"
+    print(config_dir)
+    config_file = f"{config_dir}\\wurpm-config.py"
+    # print(config_file)
+        
+
+    print()
+
+
+    if param == "1":
+        print(f"Path to config file: { config_file }")
+        
+        if os.path.exists(config_file):
+            print("Config File has Found.")
+            with open(f"{ config_file }", "r") as CF:
+                CFtext = CF.read()
+                print()
+                print(CFtext)
+
+        elif os.path.exists(config_file) == False:
+            print("Config File not Found!")
+            #print(config_file)
+            if os.path.exists(config_dir) == False:
+                os.system(f"mkdir { config_dir }")
+            elif os.path.exists(config_dir):
+                print("Config File created...")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
