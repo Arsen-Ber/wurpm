e024a695aa184c6bb634b6bb05b6ab1859f2dfa8
Arsen-Ber <arsenber09@gmail.com>
2022-06-15 19:04:55 +0300

    main

--- wurpm-1.1-beta/functions.py
+++ wurpm-1.1-beta/functions.py
@@ -14,10 +14,11 @@ def install_package(link, package_name, download_path=f"{ pathlib.Path().resolve
         wget.download(link, f"{download_path}/{package_name}")
         print()
         print(f"Installed as: { download_path }/{ package_name }")
-    except:
-        print("Error 404")
-        print(f"URL: \"{ link }\" not Found!")
-        print(f"Package: \"{ package_name }\" not Found!")
+    except FileNotFoundError:
+        print(f"Directory \'{ download_path }\' not Found!")
+        # print("Error 404")
+        # print(f"URL: \"{ link }\" not Found!")
+        # print(f"Package: \"{ package_name }\" not Found!")
         print()
         return
 ### end func ###
@@ -27,8 +28,9 @@ def get_link(frontend_info_for_package, symbols_about_start):
     LIPL = []              #* List Install Package Link *#
     LIPL = list(frontend_info_for_package)      #* List Link IPL   #* link : 54 symbols before start  (and "\r" "\n" in start from bs4 parser)*#
     del LIPL[0:symbols_about_start]
-    STRIPL = ''.join(LIPL) #* String Link IPL (link to install package)
-    return STRIPL
+    STRIPL = ''.join(LIPL) #* String Link IPL (link to install package) *#
+    STRIPLS = STRIPL.strip() #* STRPL strip *#
+    return STRIPLS
 ### end func ###
 
 
